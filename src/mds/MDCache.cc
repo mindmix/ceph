c2e1f267287657f1f9699d1aa34509a02bacbd4e
Greg Farnum <greg@inktank.com>
2014-03-12 15:56:11 -0700

    SQUASH: MDCache: cast to proper type from MutationRef
    Signed-off-by: Greg Farnum <greg@inktank.com>

--- src/mds/MDCache.cc
+++ src/mds/MDCache.cc
@@ -2014,7 +2014,9 @@ void MDCache::predirty_journal_parents(MutationRef& mut, EMetaBlob *blob,
 	mut->wrlocks.count(&pin->nestlock) == 0 &&
 	(!pin->versionlock.can_wrlock() ||                   // make sure we can take versionlock, too
 	 //true
-	 !mds->locker->wrlock_start(&pin->nestlock, static_cast<MDRequest*>(mut), true) // can cast only because i'm passing nowait=true
+	 !mds->locker->wrlock_start(&pin->nestlock,
+				    ceph::static_pointer_cast<MutationImpl,MDRequestImpl>(mut),
+				    true) // can cast only because i'm passing nowait=true
 	 )) {  // ** do not initiate.. see above comment **
       dout(10) << "predirty_journal_parents can't wrlock one of " << pin->versionlock << " or " << pin->nestlock
 	       << " on " << *pin << dendl;
